2c4cbb5d9c82860c99738b3dad38c0a3e4aa67c0
Arup Deb <arupdeb20010@gmail.com>
2022-06-27 16:07:30 +0530

    Update 24 reverse String.cpp

--- 24 reverse String.cpp
+++ 24 reverse String.cpp
@@ -22,4 +22,21 @@ int main()
     }
 
     cout<<"Answer is: "<<ans<<endl;
+     /* real
+    //return the address of the string
+char* reverse(char *S, int len)
+{
+     stack<char> s;
+   for(int i=0;i<len;i++)
+       s.push(S[i]);
+       
+   char *str = new char[len];
+   int j=0;
+   while(!s.empty())
+   {
+       str[j++]=s.top();
+       s.pop();
+   }
+   return str;
+    */
 }
