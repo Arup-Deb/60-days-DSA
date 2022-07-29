5042ef6185067c36bc75e41eb0c224c9069bccc9
Arup Deb <arupdeb20010@gmail.com>
2022-07-29 12:14:48 +0530

    Update 94 Introduction of BST.cpp

--- 94 Introduction of BST.cpp
+++ 94 Introduction of BST.cpp
@@ -97,6 +97,88 @@ Node* insertIntoBST(Node* &root, int d){
         //left part me insert karna hee
         root->left=insertIntoBST(root->left,d);
     }
+
+    return root;
+}
+
+Node* minVal(Node* root) {
+    Node* temp=root;
+
+    while(temp->left!=NULL)
+    {
+        temp=temp->left;
+    }
+    return temp;
+
+}
+
+Node* maxVal(Node* root) {
+    Node* temp=root;
+
+    while(temp->right!=NULL)
+    {
+        temp=temp->right;
+    }
+    return temp;
+
+}
+
+Node* deleteFromBST(Node* root,int val)
+{
+    //base case
+    if(root==NULL)
+    {
+        return root;
+    }
+
+    if(root==NULL)
+    {
+        return root;
+    }
+
+    if(root->data == val){
+        //0 child
+if(root->left == NULL && root->right == NULL) {
+    delete root;
+    return NULL;
+}
+        //1 child
+
+
+//leftchild
+if(root->left!=NULL && root->right == NULL) {
+    Node* temp = root->left;
+    delete root;
+    return temp;
+}
+
+//right child
+if(root->left!=NULL && root->right == NULL) {
+    Node* temp = root->right;
+    delete root;
+    return temp;
+}
+
+        //2 child
+if(root->left!=NULL && root->right == NULL) {
+int mini = minVal(root->right)->data;
+root->data = mini;
+root->right = deleteFromBST(root->right,mini);
+return root;
+
+}
+
+    }
+    else if(root->data>val){
+        //left part me jao
+        root->left = deleteFromBST(root->left,val);
+        return root;
+    }
+    else{
+        //right part me jao
+        root->right = deleteFromBST(root->right,val);
+        return root;
+    }
 }
 void takeInput(Node* &root) {
     int data;
@@ -116,6 +198,19 @@ takeInput(root);
 
 cout<<"Printing the BST"<<endl;
 levelOrderTraversal(root);
+
+cout<<"Printing inorder"<<endl;
+inorder(root);
+
+cout<<"Printing preorde"<<endl;
+preorder(root); 
+
+cout<<"Printing postorder"<<endl;
+postorder(root);
+
+cout<<"Min value is"<<minVal(root)->data<<endl;
+
+cout<<"Max value is"<<maxVal(root)->data<<endl;
 
 return 0;
 
