4b0fcbac02495788c35633eb1cc8cca8c6de49d6
Arup Deb <arupdeb20010@gmail.com>
2022-08-04 14:20:24 +0530

    Add files via upload

---
+++ 110 Trapping Rain Water.cpp
@@ -0,0 +1,71 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+
+    // Function to find the trapped water between the blocks.
+    public:
+    long long trappingWater(int arr[], int n)
+    {
+        
+        int low = 0;
+        int high = n-1;
+        
+        int start =INT_MIN;
+        int last = INT_MIN;
+        
+        long long res =0;
+        while(low <= high)
+        {
+            start = max(start,arr[low]);
+            last = max(last,arr[high]);
+            
+            if(arr[low] < arr[high])
+            {
+                res += start - arr[low];
+                low++;
+            }
+            else
+            {
+                res += last - arr[high];
+                high--;
+            }
+        }
+        return res;
+    }
+        
+    
+};
+
+// { Driver Code Starts.
+
+int main(){
+    
+    int t;
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        int n;
+        
+        //size of array
+        cin >> n;
+        
+        int a[n];
+        
+        //adding elements to the array
+        for(int i =0;i<n;i++){
+            cin >> a[i];            
+        }
+        Solution obj;
+        //calling trappingWater() function
+        cout << obj.trappingWater(a, n) << endl;
+        
+    }
+    
+    return 0;
+}  // } Driver Code Ends
